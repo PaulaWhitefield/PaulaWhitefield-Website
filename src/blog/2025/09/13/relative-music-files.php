d4a93f605da0c194ee1f737f7ce29dcbbe5e5424
PaulaWhitefield <paula.whitefield@themailbox.net>
2025-09-13 02:03:58 +0100

    Added new blog post: Relative Music Files

---
+++ src/blog/2025/09/13/relative-music-files.php
@@ -0,0 +1,18 @@
+---
+article_title: Relative Music Files
+article_description: Faster and easier access to my music.
+article_author: Paula Whitefield
+article_date: 2025-09-13
+article_time: 02:00
+
+current_nav_item: blog
+layout: src/_includes/base.php
+---
+
+<p>Since my music files are rather small, I figured why not just move them in with my website files.</p>
+
+<p>This means everyone can enjoy faster access to my music, because there won't be the extra DNS resolution that occurs when accessing the files on remote storage, and also easier because anywhere my website is, my music will also be there too.</p>
+
+<p>I just tested it out before going to sleep tonight, and figured I'd see how it goes. I may have to change this decision in the future, but for now, enjoy.</p>
+
+<p>&lt;3</p>
